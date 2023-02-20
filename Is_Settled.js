f889c74ba4e36e10ed5e729e9b67776bbbc2bb8c
= <=>
2023-02-20 14:53:53 -0700

    First commit

---
+++ Is_Settled.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Is_Settled
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Finds what Authorizations have a corresponding batch
+// @author       Susan H
+// @match        https://translink.transfirst.com/content/MerchantReports/MerchantAuthorization.aspx?ReportId=12&MenuId=1
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=transfirst.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
