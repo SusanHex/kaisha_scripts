38f9296b4b5d9f0928b1f6f3b3fdf69be38e61ce
= <=>
2023-03-30 11:35:06 -0600

    more general match criteria

--- Is_Settled.js
+++ Is_Settled.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Finds what Authorizations have a corresponding batch
 // @author       Susan H
-// @match        https://translink.transfirst.com/content/MerchantReports/MerchantAuthorization.aspx?ReportId=12&MenuId=1
+// @match        https://translink.transfirst.com/content/MerchantReports/MerchantAuthorization.aspx*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=transfirst.com
 // @grant        none
 // ==/UserScript==
