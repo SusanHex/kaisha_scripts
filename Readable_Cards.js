cc4b7058b61ae81cd7298600e9b03caf47332684
= <=>
2024-01-09 15:50:31 -0700

    added convenient clipboard feature

--- Readable_Cards.js
+++ Readable_Cards.js
@@ -6,17 +6,36 @@
 // @author       Susan H
 // @match        https://www.mreports.com/ac/authInquiry.do
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mreports.com
-// @grant        none
+// @grant        GM_setClipboard
 // ==/UserScript==
 
 (function() {
     'use strict';
-//document.querySelector("img[src=\"/ac/images/refresh.gif\"]").onclick = () => {
-//    console.log("Hello world");
-//}
-    // Your code here...
+
     document.querySelectorAll("td[width=\"10%\"] > span").forEach(elm => {
-//console.log(elm.innerText);
-      elm.innerText = elm.innerText.replace(/.{4}(?=.+)/g, '$&\-');
+        if (elm.parentElement.parentElement.childElementCount === 17) {
+            elm.innerText = elm.innerText.replace(/.{4}(?=.+)/g, '$&\-');
+            elm.parentElement.nextElementSibling.addEventListener("click", function(e) {
+                let el = e.target;
+                let auth_date = el.parentElement.children[12].innerText;
+                let auth_amount = el.parentElement.children[7].innerText;
+                let auth_num = el.parentElement.children[5].innerText.replace("-", "");
+                let last_four = auth_num.slice(-4);
+                let cc_brand = "";
+                if (auth_num[0] === "6") {
+                    cc_brand = "DS";
+                }
+                else if (auth_num[0] === "5") {
+                    cc_brand = "MC";
+                }
+                else if (auth_num[0] === "4") {
+                    cc_brand = "VS";
+                }
+                else if (auth_num[0] === "3") {
+                    cc_brand = "AM";
+                };
+                GM_setClipboard(`${auth_date} \$${auth_amount} ${cc_brand}#${last_four}`, "text", () => console.log("Coppied to clipboard"));
+            });
+        };
     });
 })();
