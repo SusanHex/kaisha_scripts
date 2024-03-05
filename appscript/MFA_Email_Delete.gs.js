847941789e0c5b017fa0ed87f0c2b927eb0b47c8
SusanHex <suehex4027@gmail.com>
2024-03-05 09:48:39 -0700

    Add Email MFA Delete AppScript

---
+++ appscript/MFA_Email_Delete.gs.js
@@ -0,0 +1,9 @@
+function deleteMFAEmails() {
+    let mfa_label = GmailApp.getUserLabelByName("MFA/OTP");
+    for (let thread of mfa_label.getThreads()) {
+      Logger.log(`Found thread: ${thread.getFirstMessageSubject()}`);
+      thread.moveToTrash();
+    }
+  
+  }
+  
