b3ff837fcbe8fe67934f8f1cae89a02841c3e3e7
SusanHex <suehex4027@gmail.com>
2024-03-19 10:58:38 -0600

    Finish Quick_To_Capture navigation script

--- Quick_To_Capture.user.js
+++ Quick_To_Capture.user.js
@@ -5,11 +5,15 @@
 // @description  Make navigating faster!
 // @author       SusanHex
 // @match        https://www.mreports.com/cbos/login.do*
+// @run-at document-idle
 // @grant        none
 // ==/UserScript==
 
 (function () {
   'use strict';
+  appSubmitForm.appid.value = '4';
+  appSubmitForm.targetPage.value = '';
+  appSubmitForm.url.value = '/ac/login.do';
   appSubmitForm.target = '';
   appSubmitForm.submit();
 })();
